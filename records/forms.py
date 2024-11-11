93967add80bb00df134309f5391cb7ae7ca89ef2
Thanawas-Sirilertsathit <thanawas.s@ku.th>
2024-11-11 22:30:16 +0700

    Disco tech theme applied

--- records/forms.py
+++ records/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import Pawnshop, Record
+from django.core.exceptions import ValidationError
 
 
 class RecordForm(forms.ModelForm):
@@ -10,6 +11,17 @@ class RecordForm(forms.ModelForm):
             'start_date': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
             'end_date': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
         }
+
+    def clean(self):
+        """Validate data."""
+        cleaned_data = super().clean()
+        start_date = cleaned_data.get("start_date")
+        end_date = cleaned_data.get("end_date")
+
+        if start_date and end_date and end_date < start_date:
+            raise ValidationError("End date cannot be earlier than start date.")
+        
+        return cleaned_data
 
 
 class PawnshopForm(forms.ModelForm):
